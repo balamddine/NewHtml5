55953009e66d9c79d2840cacf28e7d26c53f9c3a
balamddine <balamedidn@gmail.com>
2015-12-19 15:51:20 +0200

    commit12/19/2015-351PM

--- Websocket/Demo/HTMLClient/js/Demo.js
+++ Websocket/Demo/HTMLClient/js/Demo.js
@@ -4,25 +4,31 @@
     if ("WebSocket" in window) {
         dv_msg.removeClass("alert-danger");
         dv_msg.toggleClass("alert alert-success");
-        dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;WebSocket is supported by your Browser!");
+        dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;WebSocket is supported by your Browser!<br><br><span style='font-size:12px'>Connecting</span>");
 
         // Let us open a web socket
         var ws = new WebSocket("ws://localhost:9998/echo");
-
-        ws.onopen = function () {
+        
+        ws.onopen = function (event) {
             dv_msg.removeClass("alert-danger");
-            dv_msg.toggleClass("alert alert-success");
-            dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;You are now connected to the server!");
+            dv_msg.addClass("alert alert-success");
+            dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;You are now connected to server You can send a msg now!");
+
             // Web Socket is connected, send data using send()
-            ws.send("Message to send");
-            dv_msg.html ("Message is sent...");
+           // ws.send("Hi From client");
+            //dv_msg.html ("Message is sent...");
         };
 
         ws.onmessage = function (evt) {
             var received_msg = evt.data;
             dv_msg.html("Message is received... : " + received_msg);
         };
-
+        // Handle any errors that occur.
+        ws.onerror = function (error) {
+            dv_msg.removeClass("alert-success");
+            dv_msg.addClass("alert alert-danger");
+            dv_msg.html("<span class='glyphicon glyphicon-remove' ></span>&nbsp;&nbsp;WebSocket Error: " + error);
+         };
         ws.onclose = function () {
             // websocket is closed.
             dv_msg.removeClass("alert-success");
@@ -35,10 +41,14 @@
     else {
         // The browser doesn't support WebSocket
         dv_msg.removeClass("alert-success");
-        dv_msg.toggleClass("alert alert-danger");
+        dv_msg.addClass("alert alert-danger");
         dv_msg.html("<span class='glyphicon glyphicon-remove' ></span>&nbsp;&nbsp;WebSocket is NOT supported by your Browser!");
     }
 
-
+    $("#btnsend").click(function () {
+        var v = $("#txtSend").val();
+        ws.send(v);
+        $("#txtSend").val("");
+    });
 
 });
--- Websocket/Demo/Server/WebSocketServer/WebSocketServer/Server.cs
+++ Websocket/Demo/Server/WebSocketServer/WebSocketServer/Server.cs
@@ -4,23 +4,40 @@ using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 class Server
 {
-
-    TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9998);
+    const string SERVER_IP = "127.0.0.1";
+    const int  SERVER_PORT = 9998;
+    TcpListener server = new TcpListener(IPAddress.Parse(SERVER_IP), SERVER_PORT);
     public void Start ()
     {
         server.Start();
-        Console.WriteLine("Server has started on 127.0.0.1:9998.{0}Waiting for a connection...", Environment.NewLine);
-        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Server has started on "+SERVER_IP+":"+SERVER_PORT+".{0}Waiting for a connection...", Environment.NewLine);
+        TcpClient client;
+        while (true) // Add your exit flag here
+        {
+            client = server.AcceptTcpClient();
+            ThreadPool.QueueUserWorkItem(ThreadProc, client);
+        }
+        
+
+       
+        
+    }
+
+    private void ThreadProc(object obj)
+    {
         Console.WriteLine("A client connected.");
-
+        TcpClient client = (TcpClient)obj;
         NetworkStream stream = client.GetStream();
-
         //enter to an infinite cycle to be able to handle every change in stream
         while (true)
         {
+
             while (!stream.DataAvailable) ;
 
             Byte[] bytes = new Byte[client.Available];
@@ -29,7 +46,7 @@ class Server
             // translate bytes of request to string
             string data = Encoding.UTF8.GetString(bytes);
 
-            if (new Regex("^GET").IsMatch(data))
+            if (new Regex("^GET").IsMatch(data)) // Handshaking protocol
             {
                 Byte[] response = Encoding.UTF8.GetBytes("HTTP/1.1 101 Switching Protocols" + Environment.NewLine
                     + "Connection: Upgrade" + Environment.NewLine
@@ -44,8 +61,40 @@ class Server
                     + Environment.NewLine);
 
                 stream.Write(response, 0, response.Length);
+
             }
+            else
+            {
+                string msg = DecodeMessage(bytes);
+                Console.WriteLine(msg);
+
+            }
+
+            //Console.WriteLine(ByteToString(bytes));
+        }
+    }
+
+    private String DecodeMessage(Byte[] bytes)
+    {
+        String incomingData = String.Empty;
+        Byte secondByte = bytes[1];
+        Int32 dataLength = secondByte & 127;
+        Int32 indexFirstMask = 2;
+        if (dataLength == 126)
+            indexFirstMask = 4;
+        else if (dataLength == 127)
+            indexFirstMask = 10;
+
+        IEnumerable<Byte> keys = bytes.Skip(indexFirstMask).Take(4);
+        Int32 indexFirstDataByte = indexFirstMask + 4;
+
+        Byte[] decoded = new Byte[bytes.Length - indexFirstDataByte];
+        for (Int32 i = indexFirstDataByte, j = 0; i < bytes.Length; i++, j++)
+        {
+            decoded[j] = (Byte)(bytes[i] ^ keys.ElementAt(j % 4));
         }
 
+        return incomingData = Encoding.UTF8.GetString(decoded, 0, decoded.Length);
     }
+    
 }
