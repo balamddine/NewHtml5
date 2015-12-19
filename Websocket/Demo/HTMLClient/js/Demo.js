d86726add553cce4ad7c56cb17b42acdb64ad7fc
balamddine <balamedidn@gmail.com>
2015-12-19 13:18:30 +0200

    commit12/19/2015-118PM

--- Websocket/Demo/HTMLClient/js/Demo.js
+++ Websocket/Demo/HTMLClient/js/Demo.js
@@ -2,14 +2,17 @@
 
     var dv_msg = $("#dv_msg");
     if ("WebSocket" in window) {
-        dv_msg.css("color","#65b042");
-        dv_msg.html("WebSocket is supported by your Browser!");
+        dv_msg.removeClass("alert-danger");
+        dv_msg.toggleClass("alert alert-success");
+        dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;WebSocket is supported by your Browser!");
 
         // Let us open a web socket
         var ws = new WebSocket("ws://localhost:9998/echo");
 
         ws.onopen = function () {
-            dv_msg.html("You are now connected !");
+            dv_msg.removeClass("alert-danger");
+            dv_msg.toggleClass("alert alert-success");
+            dv_msg.html("<span class='glyphicon glyphicon-ok' ></span>&nbsp;&nbsp;You are now connected to the server!");
             // Web Socket is connected, send data using send()
             ws.send("Message to send");
             dv_msg.html ("Message is sent...");
@@ -22,14 +25,18 @@
 
         ws.onclose = function () {
             // websocket is closed.
-            dv_msg.css("color", "red");
-            dv_msg.html("Connection is closed...the server may be down ");
+            dv_msg.removeClass("alert-success");
+            dv_msg.addClass("alert alert-danger");
+            dv_msg.html("<span class='glyphicon glyphicon-remove' ></span>&nbsp;&nbsp;Connection is closed...the server may be down ! please try again later");
+            
         };
     }
 
     else {
         // The browser doesn't support WebSocket
-        dv_msg.html("WebSocket NOT supported by your Browser!");
+        dv_msg.removeClass("alert-success");
+        dv_msg.toggleClass("alert alert-danger");
+        dv_msg.html("<span class='glyphicon glyphicon-remove' ></span>&nbsp;&nbsp;WebSocket is NOT supported by your Browser!");
     }
 
 
